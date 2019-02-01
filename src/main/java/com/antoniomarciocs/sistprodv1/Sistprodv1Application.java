1d1d9d463fbabb220a7e33e7958e5132c78a1ccc
Antonio Marcio <antoniomarciocs@gmail.com>
2019-02-01 20:59:10 -0300

    Projeto criado

---
+++ src/main/java/com/antoniomarciocs/sistprodv1/Sistprodv1Application.java
@@ -0,0 +1,14 @@
+package com.antoniomarciocs.sistprodv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sistprodv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sistprodv1Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/antoniomarciocs/sistprodv1/Sistprodv1ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.antoniomarciocs.sistprodv1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sistprodv1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
