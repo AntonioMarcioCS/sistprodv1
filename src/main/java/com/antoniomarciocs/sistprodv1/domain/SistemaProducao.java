8b872d8b7c396db1c97bbee82eb2f1003a3c300a
Antonio Marcio <antoniomarciocs@gmail.com>
2019-02-02 22:33:58 -0300

    Banco H2 - Criação automática do banco

--- src/main/java/com/antoniomarciocs/sistprodv1/domain/SistemaProducao.java
+++ src/main/java/com/antoniomarciocs/sistprodv1/domain/SistemaProducao.java
@@ -1,10 +1,19 @@
 package com.antoniomarciocs.sistprodv1.domain;
 
 import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 //import java.util.Date;
 
+@Entity
 public class SistemaProducao implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	//private Date data;
