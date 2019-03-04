63f9bc9ef0a2a2ca1b8af484ca5bb61e33729947
Antonio Marcio <antoniomarciocs@gmail.com>
2019-03-04 10:58:00 -0300

    CRUD SistemaProducao c erros no POST e no PUT, Delete com Exception Ok

--- src/main/java/com/antoniomarciocs/sistprodv1/resources/SistemaProducaoResource.java
+++ src/main/java/com/antoniomarciocs/sistprodv1/resources/SistemaProducaoResource.java
@@ -1,11 +1,16 @@
 package com.antoniomarciocs.sistprodv1.resources;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.antoniomarciocs.sistprodv1.domain.SistemaProducao;
 import com.antoniomarciocs.sistprodv1.services.SistemaProducaoService;
@@ -18,9 +23,37 @@ public class SistemaProducaoResource {
 	private  SistemaProducaoService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<SistemaProducao> find(@PathVariable Integer id) {
 		SistemaProducao obj = service.buscar(id);
 		
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody SistemaProducao obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody SistemaProducao obj, Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<SistemaProducao> delete(@PathVariable Integer id) {
+		service.delete(id);		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<SistemaProducao>> findAll() {
+		List<SistemaProducao> list = service.buscarTodos();
+		return ResponseEntity.ok().body(list);
+	}
+	
 }
--- src/main/java/com/antoniomarciocs/sistprodv1/services/SistemaProducaoService.java
+++ src/main/java/com/antoniomarciocs/sistprodv1/services/SistemaProducaoService.java
@@ -1,11 +1,14 @@
 package com.antoniomarciocs.sistprodv1.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import com.antoniomarciocs.sistprodv1.domain.SistemaProducao;
 import com.antoniomarciocs.sistprodv1.repositories.SistemaProducaoRepository;
+import com.antoniomarciocs.sistprodv1.services.exceptions.DataIntegrityException;
 import com.antoniomarciocs.sistprodv1.services.exceptions.ObjectNotFountException;
 
 @Service
@@ -19,4 +22,28 @@ public class SistemaProducaoService {
 		return obj.orElseThrow(() -> new ObjectNotFountException("Objeto não encontrado! ID:"+id+ " Nome:"
 		+SistemaProducao.class.getName()));
 	}
+	
+	public SistemaProducao insert(SistemaProducao obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public SistemaProducao update(SistemaProducao obj) {
+		buscar(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		buscar(id);
+		try {
+			repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um Sistema, ele possui várias dependências");
+		}
+	}
+	
+	public List<SistemaProducao> buscarTodos(){
+		return repo.findAll();
+	}
+	
 }
